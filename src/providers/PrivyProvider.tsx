405506e81a80180e011d54f03005ff2e58bda1c4
Mide001 <olamideisreal5@gmail.com>
2025-06-13 22:07:36 +0100

    refactor: improve provider configuration

--- src/providers/PrivyProvider.tsx
+++ src/providers/PrivyProvider.tsx
@@ -5,8 +5,8 @@ import { PrivyProvider } from "@privy-io/react-auth";
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <PrivyProvider
-      appId="cmbs4506m017ijo0nmnw7hx9j"
-      clientId="client-WY6MCeXPXMRFwngxKiaUbb1HwY5eu7kTsNz2DYcSvkmZL"
+      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID || ''}
+      clientId={process.env.PRIVY_CLIENT_ID || ''}
       config={{
         embeddedWallets: {
           ethereum: {
